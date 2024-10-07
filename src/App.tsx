b780afaedbb0637d0c9cf10901eb3fae9064be89
Miłosz Olejnik <milosz.olejnik@tuta.io>
2024-10-07 21:30:43 +0200

    added Router and error 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HomePage } from './pages/homePage/HomePage';
+import NotFoundPage from './pages/errorPages/NotFoundPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <div>Hello</div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
